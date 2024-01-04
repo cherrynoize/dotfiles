8cd54319e6ef5fd1cb52f0264d7f59cd9db6bd7f
cherrynoize <cherrynoize9987@outlook.com>
2024-01-04 15:20:31 +0100

    update

--- .local/bin/suspend-actions.sh
+++ .local/bin/suspend-actions.sh
@@ -5,6 +5,8 @@
 dunstctl set-paused true & # pause notifications
 /usr/bin/xset -dpms & # disable dpms
 (screen-off & # turn off screen
-  lockscreen & # lock screen
+  if which lockscreen >/dev/null 2>&1; then
+    lockscreen & # lock screen
+  fi
   sleep .3; # adjust sleep delay to avoid screenlock flashing before suspend
   screen-on)& # turn screen back on
