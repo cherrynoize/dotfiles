99ac2b634ab1f06b742e656f54a9f433f96f68bf
cherrynoize <cherrynoize9987@outlook.com>
2025-06-09 00:00:02 +0200

    auto sync (vcs-sync: 2025-06-09 00:00)

--- .github/install.sh
+++ .github/install.sh
@@ -7,4 +7,7 @@ cp -n ./env ~/.env # install environment file
 cp -r -n ../bin/* ~/bin # install binaries
 fix-wal-templates # create wal template links
 fix-wpg-templates # create wpg template links
+
+sd ln -s bin/update-mirrorlist /usr/local/bin/update-mirrolist # make update-mirrorlist reachable for cron
+
 update-system # fetch latest update
