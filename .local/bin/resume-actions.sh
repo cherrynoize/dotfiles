6d00ba4858b36a69296c24b9c476024a3e8300ed
cherrynoize <cherrynoize9987@outlook.com>
2026-05-25 17:16:19 +0200

    update

--- .local/bin/resume-actions.sh
+++ .local/bin/resume-actions.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-# after system resume from sleep
-if which lockscreen >/dev/null 2>&1; then
-  lockscreen &
+# wait for screen to be locked
+while ! pgrep screenlock; do sleep .3 > /dev/null; done
 
-  if [ "$(get-display-server)" = "wayland" ]; then
-    sleep .65
-  else
-    sleep .3
-  fi
-fi
-screen-on # turn screen back on
+"$HOME/bin/screen-on" # turn screen back on
 
-# wait until screen is unlocked
-while pgrep lockscreen; do sleep 3 > /dev/null; done
+# wait for screen to be unlocked
+# while pgrep screenlock; do sleep 3 > /dev/null; done
 
-dunstctl set-paused false & # resume notifications
+# /usr/bin/dunstctl set-paused false & # resume notifications
--- .local/bin/suspend-actions.sh
+++ .local/bin/suspend-actions.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# actions to be run before system suspend
 "$HOME/bin/notify-timer" -k 1111 2>/dev/null & # kill power management timers
 /usr/bin/playerctl pause 2>/dev/null || true # stop music or ignore if not playing
-dunstctl set-paused true & # pause notifications
-screen-off & # turn off screen
+/usr/bin/dunstctl set-paused true & # pause notifications
+"$HOME/bin/screen-off" # turn screen off to prevent screenlock flashing
