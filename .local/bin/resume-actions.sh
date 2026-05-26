fceaeb87964c573c152d6d6d65d2627c6f622f85
cherrynoize <cherrynoize9987@outlook.com>
2026-05-26 03:49:56 +0200

    update

--- .local/bin/resume-actions.sh
+++ .local/bin/resume-actions.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# wait for screen to be locked
-while ! pgrep screenlock; do sleep .3 > /dev/null; done
 
 "$HOME/bin/screen-on" # turn screen back on
 
--- .local/bin/suspend-actions.sh
+++ .local/bin/suspend-actions.sh
@@ -2,4 +2,4 @@
 "$HOME/bin/notify-timer" -k 1111 2>/dev/null & # kill power management timers
 /usr/bin/playerctl pause 2>/dev/null || true # stop music or ignore if not playing
 /usr/bin/dunstctl set-paused true & # pause notifications
-"$HOME/bin/screen-off" # turn screen off to prevent screenlock flashing
+"$HOME/bin/screen-off" & # turn screen off to prevent screenlock flashing
