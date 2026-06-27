0df2941f64c95c887b5fe3984d0ae3b3c058dd58
cherrynoize <cherrynoize9987@outlook.com>
2026-06-28 00:00:17 +0200

    auto sync (vcs-sync: 2026-06-28 00:00)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -209,7 +209,8 @@ if status is-interactive
   abbr destroy 'kill -9'
   abbr transfer 'rsync -aPhv --info=progress2 --hard-links --ignore-existing' # transfer SOURCE DEST
   abbr rclone 'rclone --password-command "pass show cloud/rclone" -P'
-  abbr pass 'unlock >/dev/null 2>&1;pass'
+  abbr u 'unlock && vcs-sync'
+  abbr pass 'unlock >/dev/null 2>&1 && pass'
   abbr resize 'convert -resize '
   abbr --set-cursor rotate 'set img "%s";set tmpdst /tmp/"$(basename "$img")";convert "$img" -rotate 90 "$tmpdst";mv "$tmpdst" "$img"'
   abbr --set-cursor country 'curl -s "http://country.io/names.json" | jq "."'
