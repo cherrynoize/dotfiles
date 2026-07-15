8f4d0f94e4a1bf3f97c2addd6d36d2a94b4249cd
cherrynoize <cherrynoize9987@outlook.com>
2026-07-16 00:00:01 +0200

    auto sync (vcs-sync: 2026-07-16 00:00)

--- .local/bin/resume-actions.sh
+++ .local/bin/resume-actions.sh
@@ -5,7 +5,9 @@
 # wait for screen to be unlocked
 # while pgrep screenlock; do sleep 3 > /dev/null; done
 
-for i in $(seq 1 10); do
-  /usr/bin/dunstctl set-paused false && break
-  sleep 3
-done &
+if pidof dunst; then
+  for i in $(seq 1 10); do
+    sleep 3
+    /usr/bin/dunstctl set-paused false && break
+  done &
+fi
